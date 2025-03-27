2e8aaa52449aeb987c334fb7490461c9106f8159
divyapujari04 <158605674+divyapujari04@users.noreply.github.com>
2025-03-27 16:18:49 +0530

    Add files via upload

---
+++ 2223517.py
@@ -0,0 +1,12 @@
+def run(path) :     
+    # import modules here 
+    import pandas as pd
+    import re
+
+def absent(attendance_df):
+    
+    attendance_df = attendance_df.sort_values(by=['student_id', 'attendance_date'])
+    
+    result = []
+    
+                                                                                    
